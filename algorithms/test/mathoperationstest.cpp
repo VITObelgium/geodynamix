01e99c61925d41bb259ee10db8ad64a6881eb64b
Dirk Vanden Boer <dirk.vdb@gmail.com>
2022-05-30 15:21:56 +0200

    ClipTest: don't use nan nodata as it also verifies integer rasters

--- algorithms/test/mathoperationstest.cpp
+++ algorithms/test/mathoperationstest.cpp
@@ -13,39 +13,41 @@ TEST_CASE_TEMPLATE("Math operations", TypeParam, RasterTypes)
     using Raster = typename TypeParam::raster;
     if (!typeSupported<T>()) return;
 
-    auto nan = std::numeric_limits<double>::quiet_NaN();
-
     SUBCASE("abs")
     {
+        double nod = -9999.0;
+
         const std::vector<double> v = {
-            nan, 1.0, -1.0,
-            -4.0, nan, -2.0,
-            0.0, 2.0, nan};
+            nod, 1.0, -1.0,
+            -4.0, nod, -2.0,
+            0.0, 2.0, nod};
 
         const std::vector<double> exp = {
-            nan, 1.0, 1.0,
-            4.0, nan, 2.0,
-            0.0, 2.0, nan};
+            nod, 1.0, 1.0,
+            4.0, nod, 2.0,
+            0.0, 2.0, nod};
 
-        Raster raster(RasterMetadata(3, 3, nan), convertTo<T>(v));
-        Raster expected(RasterMetadata(3, 3, nan), convertTo<T>(exp));
+        Raster raster(RasterMetadata(3, 3, nod), convertTo<T>(v));
+        Raster expected(RasterMetadata(3, 3, nod), convertTo<T>(exp));
         CHECK_RASTER_EQ(expected, gdx::abs(raster));
     }
 
     SUBCASE("clip")
     {
+        double nod = -9999.0;
+
         const std::vector<double> input = {
-            nan, 0.0, -1.0,
-            1.0, nan, 2.0,
-            3.0, 4.0, nan};
+            nod, 0.0, -1.0,
+            1.0, nod, 2.0,
+            3.0, 4.0, nod};
 
         const std::vector<double> expectedData = {
-            nan, 1.0, 1.0,
-            1.0, nan, 2.0,
-            3.0, 3.0, nan};
+            nod, 1.0, 1.0,
+            1.0, nod, 2.0,
+            3.0, 3.0, nod};
 
-        Raster raster(RasterMetadata(3, 3, nan), convertTo<T>(input));
-        Raster expected(RasterMetadata(3, 3, nan), convertTo<T>(expectedData));
+        Raster raster(RasterMetadata(3, 3, nod), convertTo<T>(input));
+        Raster expected(RasterMetadata(3, 3, nod), convertTo<T>(expectedData));
         CHECK_RASTER_EQ(expected, gdx::clip(raster, T(1), T(3)));
     }
 }
