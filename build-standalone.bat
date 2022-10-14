6c8567c0558d3da1b5847229672e917016837458
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2022-10-14 11:08:43 +0200

    also build 3.7

--- build-standalone.bat
+++ build-standalone.bat
@@ -3,7 +3,9 @@ mkdir standalone
 python build.py --dist --python-path=c:\python310\python.exe --build-dir=gdx-310
 python build.py --dist --python-path=c:\python39\python.exe --build-dir=gdx-39
 python build.py --dist --python-path=c:\python38\python.exe --build-dir=gdx-38
+python build.py --dist --python-path=c:\python37\python.exe --build-dir=gdx-37
 
 cp ./build/gdx-310-x64-windows-static-vs2022-dist/Release/geodynamix.cp310-win_amd64.pyd ./standalone
 cp ./build/gdx-39-x64-windows-static-vs2022-dist/Release/geodynamix.cp39-win_amd64.pyd ./standalone
 cp ./build/gdx-38-x64-windows-static-vs2022-dist/Release/geodynamix.cp38-win_amd64.pyd ./standalone
+cp ./build/gdx-37-x64-windows-static-vs2022-dist/Release/geodynamix.cp37-win_amd64.pyd ./standalone
