30d0c82ca7655266fdeddb929ade6113aa05d71f
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-11-10 14:53:01 +0100

    vcpkg update

--- build-standalone.bat
+++ build-standalone.bat
@@ -7,3 +7,4 @@ python build.py --dist --python-path=c:\python310\python.exe --build-dir=gdx-310
 cp ./build/gdx-312-x64-windows-static-vs2022-dist/geodynamix.cp312-win_amd64.pyd ./standalone
 cp ./build/gdx-311-x64-windows-static-vs2022-dist/geodynamix.cp311-win_amd64.pyd ./standalone
 cp ./build/gdx-310-x64-windows-static-vs2022-dist/geodynamix.cp310-win_amd64.pyd ./standalone
+cp ./build/gdx-310-x64-windows-static-vs2022-dist/proj.db ./standalone
