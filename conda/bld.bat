aabab0880861e68eceb3073a8660f424d3fc02e4
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2025-01-24 16:28:17 +0100

    Simplified conda build scripts

--- conda/bld.bat
+++ conda/bld.bat
@@ -6,46 +6,6 @@ SET thirdparty_install_dir=%thirdparty_dir%/local
 mkdir %SRC_DIR%\build
 mkdir %SRC_DIR%\thirdparty
 
-cd %SRC_DIR%\thirdparty
-
-git clone --branch v4.0.0 --depth 1 https://github.com/Microsoft/GSL.git
-git clone --branch v3.0.1 --depth 1 https://github.com/HowardHinnant/date
-
-REM Anaconda pkgs/main package 'eigen' for win-64 does not include Eigen3Config.cmake, so we build Eigen ourselves.
-git clone --branch 3.3.9 --depth 1 https://gitlab.com/libeigen/eigen.git eigen
-
-cd %SRC_DIR%/thirdparty/date
-cmake -G Ninja ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_TZ_LIB=ON ^
-    -DUSE_SYSTEM_TZ_DB=ON ^
-    -DENABLE_DATE_TESTING=OFF ^
-    -DCMAKE_INSTALL_PREFIX=../local ^
-    -S . -B .
-if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
-cmake --build . --target install
-if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
-
-cd %SRC_DIR%/thirdparty/GSL
-cmake -G Ninja ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DGSL_TEST=OFF ^
-    -DCMAKE_INSTALL_PREFIX=../local ^
-    -S . -B .
-if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
-cmake --build . --target install
-if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
-
-mkdir %SRC_DIR%\thirdparty\eigen\build
-cd %SRC_DIR%\thirdparty\eigen\build
-cmake -G Ninja ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=../local ^
-    -S .. -B .
-if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
-cmake --build . --target install
-if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
-
 cd %SRC_DIR%/build
 
 cmake ^
@@ -55,12 +15,7 @@ cmake ^
     -DPACKAGE_VERSION_COMMITHASH=%GIT_FULL_HASH% ^
     -DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%CMAKE_PREFIX_PATH%" ^
-    -DCMAKE_FIND_ROOT_PATH:PATH="%SRC_DIR%\thirdparty\local" ^
     -DCMAKE_MODULE_PATH:PATH="%RECIPE_DIR%\cmake" ^
-    -Ddate_DIR="%SRC_DIR%\thirdparty\local\CMake" ^
-    -Dfmt_DIR:PATH="%SRC_DIR%\thirdparty\local\lib\cmake\fmt" ^
-    -Dspdlog_DIR:PATH="%SRC_DIR%\thirdparty\local\lib\cmake\spdlog" ^
-    -DMicrosoft.GSL_DIR="%SRC_DIR%\thirdparty\local\share\cmake\Microsoft.GSL" ^
     -DGDX_ENABLE_OPENMP=OFF ^
     -DGDX_AVX2=OFF ^
     -DGDX_ENABLE_SIMD=OFF ^
@@ -73,7 +28,6 @@ cmake ^
     -DGDX_INSTALL_DEVELOPMENT_FILES=OFF ^
     -DPython3_ROOT_DIR="%PREFIX%" ^
     -DPython3_FIND_VIRTUALENV=ONLY ^
-    --debug-find-pkg=Microsoft.GSL ^
     -S .. -B .
 if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 cmake --build . --target install
--- conda/build.sh
+++ conda/build.sh
@@ -2,31 +2,6 @@
 set -e
 
 mkdir ${SRC_DIR}/build
-mkdir ${SRC_DIR}/thirdparty
-mkdir -p ${SRC_DIR}/thirdparty/local/share/cmake
-
-cd ${SRC_DIR}/thirdparty
-
-git clone --branch v4.0.0 --depth 1 https://github.com/Microsoft/GSL.git
-git clone --branch v3.0.1 --depth 1 https://github.com/HowardHinnant/date
-
-cd ${SRC_DIR}/thirdparty/GSL
-cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DGSL_TEST=OFF \
-    -DCMAKE_INSTALL_PREFIX=../local \
-    -S . -B .
-cmake --build . --target install
-
-cd ${SRC_DIR}/thirdparty/date
-cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_TZ_LIB=ON \
-    -DUSE_SYSTEM_TZ_DB=ON \
-    -DENABLE_DATE_TESTING=OFF \
-    -DCMAKE_INSTALL_PREFIX=../local \
-    -S . -B .
-cmake --build . --target install
 
 cd ${SRC_DIR}/build
 # unset the SYSCONFIGDATA_NAME otherwise pybind11 python module extension detection fails
@@ -36,7 +11,7 @@ cmake \
     ${CMAKE_ARGS} \
     -DPACKAGE_VERSION_COMMITHASH=${GIT_FULL_HASH} \
     -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} \
-    -DCMAKE_FIND_ROOT_PATH:PATH="${SRC_DIR}\thirdparty\local;${PREFIX};${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot" \
+    -DCMAKE_FIND_ROOT_PATH:PATH="${PREFIX};${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot" \
     -DCMAKE_MODULE_PATH="${RECIPE_DIR}/cmake" \
     -DGDX_DISABLE_OPENMP=OFF \
     -DGDX_ENABLE_TOOLS=OFF \
