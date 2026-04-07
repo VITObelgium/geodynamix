b510acdbe215f569f4c01e7d0cfb19a6d46e8874
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2026-04-07 10:15:35 +0200

    Bump standalone python versions

--- build-standalone.bat
+++ build-standalone.bat
@@ -1,10 +1,9 @@
-rm -rf standalone 
+rm -rf standalone
 mkdir standalone
+python build.py --dist --python-path=c:\python314\python.exe --build-dir=gdx-314
+python build.py --dist --python-path=c:\python313\python.exe --build-dir=gdx-313
 python build.py --dist --python-path=c:\python312\python.exe --build-dir=gdx-312
-python build.py --dist --python-path=c:\python311\python.exe --build-dir=gdx-311
-python build.py --dist --python-path=c:\python310\python.exe --build-dir=gdx-310
 
+cp ./build/gdx-314-x64-windows-static-vs2022-dist/geodynamix.cp314-win_amd64.pyd ./standalone
+cp ./build/gdx-313-x64-windows-static-vs2022-dist/geodynamix.cp313-win_amd64.pyd ./standalone
 cp ./build/gdx-312-x64-windows-static-vs2022-dist/geodynamix.cp312-win_amd64.pyd ./standalone
-cp ./build/gdx-311-x64-windows-static-vs2022-dist/geodynamix.cp311-win_amd64.pyd ./standalone
-cp ./build/gdx-310-x64-windows-static-vs2022-dist/geodynamix.cp310-win_amd64.pyd ./standalone
-cp ./build/gdx-310-x64-windows-static-vs2022-dist/proj.db ./standalone
